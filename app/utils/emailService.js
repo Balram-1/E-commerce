5ead0b0587f1cf7133b9f400afec3073b86a1301
Balram-1 <balrampreet116699@gmail.com>
2026-04-08 20:39:50 +0530

    REPLCAED gmail sender with outlook/hotmail

--- app/utils/emailService.js
+++ app/utils/emailService.js
@@ -1,7 +1,7 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: process.env.EMAIL_SERVICE || 'gmail',
+  service: process.env.EMAIL_SERVICE || 'hotmail',
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
